a98f62074f5b24ea916f9718806cc4bca68200ea
anubha05chouhan <129792302+anubha05chouhan@users.noreply.github.com>
2025-04-18 08:31:31 +0530

    Update ContainerServlet.java

--- Controller/ContainerServlet.java
+++ Controller/ContainerServlet.java
@@ -1,1 +1,44 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BookGenreDAOImpl;
+import dao.GenreDAOImpl;
+import model.Book;
+import model.Genre;
+
+@WebServlet("/ContainerServlet")
+public class ContainerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		try
+		{
+		GenreDAOImpl genreDao = new GenreDAOImpl();
+		ArrayList<Genre> list = (ArrayList<Genre>) genreDao.fetchAll();
+		BookGenreDAOImpl bookGenreDao = new BookGenreDAOImpl();
+		Map<String, List<Book>> genreBooksMap = bookGenreDao.fetchGenreAndBooks();
+		request.setAttribute("genreBooksMap", genreBooksMap);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("Home.jsp");
+        dispatcher.forward(request, response);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
 
