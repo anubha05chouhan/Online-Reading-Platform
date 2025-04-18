c1bf32f8ef73ab645eccdcf3b3df9dfc6932b2d8
anubha05chouhan <129792302+anubha05chouhan@users.noreply.github.com>
2025-04-18 08:32:34 +0530

    Update RegistrationServlet.java

--- Controller/RegistrationServlet.java
+++ Controller/RegistrationServlet.java
@@ -1,1 +1,50 @@
+package controller;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDAO;
+import dao.UserDAOImpl;
+import model.User;
+
+@WebServlet("/RegistrationServlet")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String email=request.getParameter("email");
+		String username=request.getParameter("username");
+		String token = UUID.randomUUID().toString().substring(0, 6);
+		User user = new User(email,username,token);
+		UserDAOImpl userDao = new UserDAOImpl();
+		try
+		{
+			if(userDao.insert(user)!=null)
+		    {
+				request.setAttribute("user", user);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/EmailServlet");
+				dispatcher.forward(request, response);
+			}
+			else 
+			{
+                request.setAttribute("error", "Email already registered!");
+                RequestDispatcher dispatcher = request.getRequestDispatcher("Registration.jsp");
+                dispatcher.forward(request, response);
+            }
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			response.sendRedirect("Registration.jsp");
+		}
+	}
+
+}
 
