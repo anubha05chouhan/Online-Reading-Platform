7c5f6f660a0d84a9089b416ff166d390c23f75f6
anubha05chouhan <129792302+anubha05chouhan@users.noreply.github.com>
2025-04-18 08:31:48 +0530

    Update Emailservlet.java

--- Controller/Emailservlet.java
+++ Controller/Emailservlet.java
@@ -1,1 +1,31 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Email;
+import model.User;
+
+@WebServlet("/EmailServlet")
+public class EmailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		User user = (User) request.getAttribute("user");
+		String token = user.getToken();
+		String message="Your token to login in the learning platform is = "+token+" dont share this with anyone";
+	    String subject="Learning Platform Confirmation";
+	    String to=user.getEmail();
+	    String from="aadarshpatel901@gmail.com";
+	    
+	    Email email = new Email(message,subject,to,from);
+        if(email.sendEmail())
+        response.sendRedirect("Login.jsp");
+	}
+}
 
